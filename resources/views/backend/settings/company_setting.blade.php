4b098b9e6a9f93e75dd856f60613fc8cd9c82742
Niloy Das <ndas4300@gmail.com>
2023-02-07 00:14:08 +0600

    Company setting page design done.

--- resources/views/backend/settings/company_setting.blade.php
+++ resources/views/backend/settings/company_setting.blade.php
@@ -36,125 +36,147 @@
         <!-- ========== title-wrapper end ========== -->
 
         <div class="row">
-            <div class="col-lg-6">
+            <div class="col-lg-10">
                 <div class="card-style settings-card-1 mb-30">
-                    <div
-                        class="
-              title
-              mb-30
-              d-flex
-              justify-content-between
-              align-items-center
-            ">
+                    <div class="title mb-30 d-flex justify-content-between align-items-center">
                         <h6>My Profile</h6>
                         <button class="border-0 bg-transparent">
                             <i class="lni lni-pencil-alt"></i>
                         </button>
                     </div>
-                    <div class="profile-info">
-                        <div class="d-flex align-items-center mb-30">
-                            <div class="profile-image">
-                                <img src="assets/images/profile/profile-1.png" alt="">
-                                <div class="update-image">
-                                    <input type="file">
-                                    <label for=""><i class="lni lni-cloud-upload"></i></label>
+                    <form action="{{ route('company-setting.update') }}" method="POST">
+                        @csrf
+                        @method('PUT')
+                        <div class="profile-info">
+                            <div class="row d-flex">
+                                <div class="col-lg-6">
+                                    <div class=" align-items-center mb-30">
+                                        <div class="profile-image mb-10">
+                                            <img src="{{ asset('') }}backend/assets/images/profile/profile-1.png"
+                                                alt="">
+                                            <div class="update-image">
+                                                <input type="file">
+                                                <label for=""><i class="lni lni-cloud-upload"></i></label>
+                                            </div>
+                                        </div>
+                                        <div class="profile-meta">
+                                            <h5 class="text-bold text-dark mb-10">Main Logo</h5>
+                                            {{-- <p class="text-sm text-gray">Web &amp; UI/UX Design</p> --}}
+                                        </div>
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="profile-meta">
-                                <h5 class="text-bold text-dark mb-10">John Doe</h5>
-                                <p class="text-sm text-gray">Web &amp; UI/UX Design</p>
-                            </div>
-                        </div>
-                        <div class="input-style-1">
-                            <label>Email</label>
-                            <input type="email" placeholder="admin@example.com" value="admin@example.com">
-                        </div>
-                        <div class="input-style-1">
-                            <label>Password</label>
-                            <input type="password" value="admin@example.com">
-                        </div>
-                        <div class="input-style-1">
-                            <label>Website</label>
-                            <input type="text" placeholder="www.uideck.com" value="www.uideck.com">
-                        </div>
-                        <div class="input-style-1">
-                            <label>Bio</label>
-                            <textarea placeholder="Write your bio here" rows="4">Crafted for – Business, Startup, SaaS, Apps, Event, Software, Agency, Resume and Portfolio. All Landing Pages comes with clean design and responsive layout. Also packed with all essential sections, elements, and features you need to launch</textarea>
-                        </div>
-                    </div>
-                </div>
-                <!-- end card -->
-            </div>
-            <!-- end col -->
-
-            <div class="col-lg-6">
-                <div class="card-style settings-card-2 mb-30">
-                    <div class="title mb-30">
-                        <h6>My Profile</h6>
-                    </div>
-                    <form action="#">
-                        <div class="row">
-                            <div class="col-12">
-                                <div class="input-style-1">
-                                    <label>Full Name</label>
-                                    <input type="text" placeholder="Full Name">
-                                </div>
-                            </div>
-                            <div class="col-12">
-                                <div class="input-style-1">
-                                    <label>Email</label>
-                                    <input type="email" placeholder="Email">
-                                </div>
-                            </div>
-                            <div class="col-12">
-                                <div class="input-style-1">
-                                    <label>Company</label>
-                                    <input type="text" placeholder="Company">
-                                </div>
-                            </div>
-                            <div class="col-12">
-                                <div class="input-style-1">
-                                    <label>Address</label>
-                                    <input type="text" placeholder="Address">
-                                </div>
-                            </div>
-                            <div class="col-xxl-4">
-                                <div class="input-style-1">
-                                    <label>City</label>
-                                    <input type="text" placeholder="City">
-                                </div>
-                            </div>
-                            <div class="col-xxl-4">
-                                <div class="input-style-1">
-                                    <label>Zip Code</label>
-                                    <input type="text" placeholder="Zip Code">
-                                </div>
-                            </div>
-                            <div class="col-xxl-4">
-                                <div class="select-style-1">
-                                    <label>Country</label>
-                                    <div class="select-position">
-                                        <select class="light-bg">
-                                            <option value="">Select category</option>
-                                            <option value="">USA</option>
-                                            <option value="">UK</option>
-                                            <option value="">Canada</option>
-                                            <option value="">India</option>
-                                            <option value="">Bangladesh</option>
-                                        </select>
+                                <div class="col-lg-6">
+                                    <div class=" align-items-center mb-30">
+                                        <div class="profile-image mb-10">
+                                            <img src="{{ asset('') }}backend/assets/images/profile/profile-1.png"
+                                                alt="">
+                                            <div class="update-image">
+                                                <input type="file">
+                                                <label for=""><i class="lni lni-cloud-upload"></i></label>
+                                            </div>
+                                        </div>
+                                        <div class="profile-meta">
+                                            <h5 class="text-bold text-dark mb-10">Footer Logo</h5>
+                                            {{-- <p class="text-sm text-gray">Web &amp; UI/UX Design</p> --}}
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                            <div class="col-12">
-                                <div class="input-style-1">
-                                    <label>About Me</label>
-                                    <textarea placeholder="Type here" rows="6"></textarea>
+                            <br />
+                            <br />
+
+
+                            <div class="row">
+
+                                <div class="col-12">
+                                    <div class="input-style-1">
+                                        <label>Company Name</label>
+                                        <input type="text" placeholder="Company Name">
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="col-12">
-                                <button class="main-btn primary-btn btn-hover">
-                                    Update Profile
-                                </button>
+                                <div class="col-12">
+                                    <div class="input-style-1">
+                                        <label>Email</label>
+                                        <input type="email" placeholder="Email">
+                                    </div>
+                                </div>
+                                <div class="col-xxl-6">
+                                    <div class="input-style-1">
+                                        <label>Mobile 1</label>
+                                        <input type="text" placeholder="Mobile Number ">
+                                    </div>
+                                </div>
+                                <div class="col-xxl-6">
+                                    <div class="input-style-1">
+                                        <label>Mobile 2</label>
+                                        <input type="text" placeholder="Mobile Number ">
+                                    </div>
+                                </div>
+                                <div class="col-12">
+                                    <div class="input-style-1">
+                                        <label>Location</label>
+                                        <input type="text" placeholder="Location">
+                                    </div>
+                                </div>
+                                <div class="col-xxl-6">
+                                    <div class="input-style-1">
+                                        <label>Facebook</label>
+                                        <input type="text" placeholder="Facebook">
+                                    </div>
+                                </div>
+                                <div class="col-xxl-6">
+                                    <div class="input-style-1">
+                                        <label>Twitter</label>
+                                        <input type="text" placeholder="Twitter">
+                                    </div>
+                                </div>
+                                <div class="col-xxl-6">
+                                    <div class="input-style-1">
+                                        <label>Instagram</label>
+                                        <input type="text" placeholder="Instagram">
+                                    </div>
+                                </div>
+                                <div class="col-xxl-6">
+                                    <div class="input-style-1">
+                                        <label>Whatsapp</label>
+                                        <input type="text" placeholder="Whatsapp">
+                                    </div>
+                                </div>
+                                <div class="col-12">
+                                    <div class="input-style-1">
+                                        <label>About Me</label>
+                                        <textarea placeholder="Type here" rows="6"></textarea>
+                                    </div>
+                                </div>
+                                <div class="col-xxl-6">
+                                    <div class="input-style-1">
+                                        <label>Meta Title</label>
+                                        <input type="text" placeholder="Meta Title">
+                                    </div>
+                                </div>
+                                <div class="col-xxl-6">
+                                    <div class="input-style-1">
+                                        <label>Meta Keywords</label>
+                                        <input type="text" placeholder="Meta Keywords">
+                                    </div>
+                                </div>
+                                <div class="col-12">
+                                    <div class="input-style-1">
+                                        <label>Meta Description</label>
+                                        <textarea placeholder="Type here" rows="4"></textarea>
+                                    </div>
+                                </div>
+                                <div class="col-12">
+                                    <div class="input-style-1">
+                                        <label>About Footer</label>
+                                        <textarea placeholder="Type here" rows="2"></textarea>
+                                    </div>
+                                </div>
+                                <div class="col-12">
+                                    <button type="submit" class="main-btn primary-btn btn-hover">
+                                        Update Settings
+                                    </button>
+                                </div>
                             </div>
                         </div>
                     </form>
@@ -167,10 +189,11 @@
     </div>
 @endsection
 {{-- @push('script')
-    <script src="{{ asset('') }}backend/assets/libs/admin-resources/bootstrap-filestyle/bootstrap-filestyle.min.js"></script>
-    <script src="backend/assets/js/pages/form-validation.init.js"></script>
+<script src="{{ asset('') }}backend/assets/libs/admin-resources/bootstrap-filestyle/bootstrap-filestyle.min.js">
+</script>
+<script src="backend/assets/js/pages/form-validation.init.js"></script>
 @endpush
 
 @push('style')
-    <link href="{{ asset('') }}backend/assets/libs/dropzone/min/dropzone.min.css" rel="stylesheet" type="text/css" />
+<link href="{{ asset('') }}backend/assets/libs/dropzone/min/dropzone.min.css" rel="stylesheet" type="text/css" />
 @endpush --}}
